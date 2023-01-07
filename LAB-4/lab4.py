ba0e71717c06838be1fb37aedbf87437dab226fe
Nisarga <niishhh22@gmail.com>
2023-01-07 08:09:58 +0530

    Modified LAB-4

--- LAB-4/lab4.py
+++ LAB-4/lab4.py
@@ -1,3 +1,10 @@
+import pandas as pd
+import numpy as np
+from numpy import log2 as log
+import pprint
+
+eps = np.finfo(float).eps
+
 def find_entropy(df):
     Class = df.keys()[-1]
     entropy = 0
@@ -6,7 +13,6 @@ def find_entropy(df):
         fraction = df[Class].value_counts()[value] / len(df[Class])
         entropy += -fraction * np.log2(fraction)
     return entropy
-
 
 def find_entropy_attribute(df, attribute):
     Class = df.keys()[-1]
@@ -26,11 +32,11 @@ def find_entropy_attribute(df, attribute):
         entropy2 += -fraction2 * entropy
     return abs(entropy2)
 
-
 def find_winner(df):
-    IG = []
-    for key in df.keys()[:-1]:
-        IG.append(find_entropy(df) - find_entropy_attribute(df, key))
+    IG = [
+        find_entropy(df) - find_entropy_attribute(df, key)
+        for key in df.keys()[:-1]
+    ]
     return df.keys()[:-1][np.argmax(IG)]
 
 def get_subtable(df, node, value):
@@ -38,30 +44,18 @@ def get_subtable(df, node, value):
 
 def buildTree(df, tree=None):
     node = find_winner(df)
-    attValue = np.unique(df[node])    
+    attValue = np.unique(df[node])
     if tree is None:
-        tree = {}
-        tree[node] = {}  
+        tree = {node: {}}
     for value in attValue:
         subtable = get_subtable(df, node, value)
         clValue, counts = np.unique(subtable["Play"], return_counts=True)
-        if len(counts) == 1:              
-            tree[node][value] = clValue[0]
-        else:
-            tree[node][value] = buildTree(subtable)
+        tree[node][value] = clValue[0] if len(counts) == 1 else buildTree(subtable)
     return tree
-
-import pandas as pd
-import numpy as np
-
-eps = np.finfo(float).eps
-from numpy import log2 as log
 
 df = pd.read_csv("/Users/darshanr/Documents/18CSL76-Artificial-Intelligence-And-Machine-Learning-LAB/LAB-4/tennis.csv")
 print("\n Given Play Tennis Data Set:\n\n", df)
 tree = buildTree(df)
-import pprint
-
 pprint.pprint(tree)
 
 test = {"Outlook": "Sunny", "Temperature": "Hot", "Humidity": "High", "Wind": "Weak"}
@@ -69,16 +63,12 @@ test = {"Outlook": "Sunny", "Temperature": "Hot", "Humidity": "High", "Wind": "W
 def func(test, tree, default=None):
     attribute = next(iter(tree))
     print(attribute)
-    if test[attribute] in tree[attribute].keys():
-        print(tree[attribute].keys())
-        print(test[attribute])
-        result = tree[attribute][test[attribute]]
-        if isinstance(result, dict):
-            return func(test, result)
-        else:
-            return result
-    else:
+    if test[attribute] not in tree[attribute].keys():
         return default
+    print(tree[attribute].keys())
+    print(test[attribute])
+    result = tree[attribute][test[attribute]]
+    return func(test, result) if isinstance(result, dict) else result
 
 ans = func(test, tree)
 print(ans)
